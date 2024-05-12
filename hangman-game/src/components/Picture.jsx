b86d15d1cf07629aeb10804fa19bcb215fb0aa5a
AkivaKn <akivakaufman@gmail.com>
2024-05-12 11:13:33 +0100

    Import svg images

--- hangman-game/src/components/Picture.jsx
+++ hangman-game/src/components/Picture.jsx
@@ -1,49 +1,30 @@
+import hangmanSix from "../assets/hangman-0.svg";
+import hangmanFive from "../assets/hangman-1.svg";
+import hangmanFour from "../assets/hangman-2.svg";
+import hangmanThree from "../assets/hangman-3.svg";
+import hangmanTwo from "../assets/hangman-4.svg";
+import hangmanOne from "../assets/hangman-5.svg";
+import hangmanZero from "../assets/hangman-6.svg";
+
 function Picture({ remainingGuesses }) {
   return (
     <section id="picture">
       <p>Remaining Guesses: {remainingGuesses} </p>
       {remainingGuesses === 6 ? (
-        <img src="../../images/hangman-0.svg" alt="gallows" id="gallows" />
+        <img src={hangmanSix} alt="gallows" id="gallows" />
       ) : remainingGuesses === 5 ? (
-        <img src="../../images/hangman-1.svg" alt="gallows" id="gallows" />
-      ) :remainingGuesses === 4 ? (
-        <img src="../../images/hangman-2.svg" alt="gallows" id="gallows" />
+        <img src={hangmanFive} alt="gallows" id="gallows" />
+      ) : remainingGuesses === 4 ? (
+        <img src={hangmanFour} alt="gallows" id="gallows" />
       ) : remainingGuesses === 3 ? (
-        <img src="../../images/hangman-3.svg" alt="gallows" id="gallows" />
-      ) :remainingGuesses === 2 ? (
-        <img src="../../images/hangman-4.svg" alt="gallows" id="gallows" />
-      ) :remainingGuesses === 1 ? (
-        <img src="../../images/hangman-5.svg" alt="gallows" id="gallows" />
+        <img src={hangmanThree} alt="gallows" id="gallows" />
+      ) : remainingGuesses === 2 ? (
+        <img src={hangmanTwo} alt="gallows" id="gallows" />
+      ) : remainingGuesses === 1 ? (
+        <img src={hangmanOne} alt="gallows" id="gallows" />
       ) : remainingGuesses === 0 ? (
-        <img src="../../images/hangman-6.svg" alt="gallows" id="gallows" />
+        <img src={hangmanZero} alt="gallows" id="gallows" />
       ) : null}
-     
-      {/* {remainingGuesses < 6 ? <div id="head"></div> : null} */}
-      {/* <div id="upper-body">
-        {remainingGuesses < 5 ? <div id="left-arm"></div> : null}
-        {remainingGuesses < 4 ? (
-          <div id="spine"></div>
-        ) : (
-          <div id="empty-spine"></div>
-        )}
-        {remainingGuesses < 3 ? (
-          <div id="right-arm"></div>
-        ) : (
-          <div className="empty"></div>
-        )}
-      </div>
-      <div id="lower-body">
-        {remainingGuesses < 2 ? (
-          <div id="left-leg"></div>
-        ) : (
-          <div className="empty"></div>
-        )}
-        {remainingGuesses < 1 ? (
-          <div id="right-leg"></div>
-        ) : (
-          <div className="empty"></div>
-        )}
-      </div> */}
     </section>
   );
 }
